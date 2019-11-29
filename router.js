cec7ca31f348fef1878fc4b12d39ef6820927418
Bruce_J <jxy941112@163.com>
2019-11-29 12:06:34 +0800

    day1~.~

--- router.js
+++ router.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import VueRouter from 'vue-router' 
 Vue.use(VueRouter)
 
+// 导入tabbar对应的component
+import home from './src/components/tabbar/Home.vue'
+import member from './src/components/tabbar/Member.vue'
+import search from './src/components/tabbar/Search.vue'
+import shopcar from './src/components/tabbar/Shopcar.vue'
 var router = new VueRouter({
   routes:[
-
-  ]
+    {path:'/',redirect:'/home'},
+    {path:'/home',component:home},
+    {path:'/member',component:member},
+    {path:'/shopcar',component:shopcar},
+    {path:'/search',component:search},
+  ],
+  linkActiveClass:'mui-active'
 })
 
 export default router
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router' 
 Vue.use(VueRouter)
 
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header);
+
 import router from '../router.js'
 
 // //mint-ui的使用
@@ -9,7 +12,19 @@ import router from '../router.js'
 // import 'mint-ui/lib/style.css' // 省略了node_modules
 // Vue.use(MintUI)
 
+// tabbar
+import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
 import App from './APP.vue'
+
+// swipe
+import { Swipe, SwipeItem } from 'mint-ui';
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+// vue-resource
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
 
 var vm = new Vue({
   el:'#app',
